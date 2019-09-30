7e957ea6518b7fc46a54363ffb0c5f2c80ace26b
Anselm Hahn <Anselm.Hahn@gmail.com>
2019-09-30 19:59:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew bundle
