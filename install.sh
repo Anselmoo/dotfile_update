0f425a60a5a78593bf66ca7d6c6eb0c45b3d61a3
Anselm Hahn <Anselm.Hahn@gmail.com>
2019-10-03 11:37:51 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,12 @@
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Script for installing missing packanges and dependecies via homebrew
+# Setting-Up vim, tmux, zsh via shell and creating alias-dotfile
+
+echo "Starting bootstrapping"
+# Check for Homebrew, install if we don't have it
+if test ! $(which brew); then
+echo "Installing homebrew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+# Update homebrew recipes
+brew update
 brew bundle
